03fe85512a412dafd42795ca17e61fc9c237f6ff
🎃 <email@sample.com>
2022-08-29 22:38:20 +0800

    完善了v-price指令

--- src/directives/handle-price.js
+++ src/directives/handle-price.js
@@ -1,26 +1,35 @@
 export default function hanldePrice(app) {
   app.directive("price", {
     mounted(el, bindlings) {
-      const preVal = el.textContent
-
-      // 参数
-      const type = bindlings.arg || "RMB"
-      // 修饰符
-      const toInt = bindlings.modifiers.toInt
-      // 值
-      let value = bindlings.value
-
-      const typeObj = { RMB: "￥", dollar: "$" }
-      const typeKeys = Object.keys(typeObj)
-
-      const typeKey = typeKeys.find((item) => item === type)
-      const typeValue = typeObj[typeKey]
-      if (toInt) {
-        value = Number(value).toFixed(0)
-      }
-      let result = `${typeValue}${value}${preVal}`
-
-      el.textContent = result
+      handlePrice(el, bindlings)
+    },
+    updated(el, bindlings) {
+      handlePrice(el, bindlings)
     },
   })
+}
+
+function handlePrice(el, bindlings) {
+  if (!bindlings.value) return
+
+  const preVal = el.textContent
+
+  // 参数
+  const type = bindlings.arg || "RMB"
+  // 修饰符
+  const toInt = bindlings.modifiers.toInt
+  // 值
+  let value = bindlings.value
+
+  const typeObj = { RMB: "￥", dollar: "$" }
+  const typeKeys = Object.keys(typeObj)
+
+  const typeKey = typeKeys.find((item) => item === type)
+  const typeValue = typeObj[typeKey]
+  if (toInt) {
+    value = Number(value).toFixed(0)
+  }
+  let result = `${typeValue}${value}${preVal}`
+
+  el.textContent = result
 }
